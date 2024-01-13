2142698ef8294af2a59ae34fae47dc325344e679
Klif <sang.wicklife@student.moringaschool.com>
2024-01-13 08:59:41 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(nameP, emailP) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name:nameP, email:emailP }),
+  };
+
+  fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((object) => appendDataToDOM(object.id))
+    .catch(function (error) {
+      appendDataToDOM(error.message);
+      alert("Oops! An error occurred.");
+      console.log(error.message);
+    });
+}
+
+function appendDataToDOM(id) {
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = `Message: ${id}`;
+  document.body.appendChild(newDiv);
+}
